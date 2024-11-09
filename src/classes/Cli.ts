99d6025887e5190cc621d30ac4e7c59a37f8c57b
Colton Doling <cmdoling@gmail.com>
2024-11-09 17:15:27 -0500

    modified the createVehicle method to include Truck and Motorbike

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -68,6 +68,10 @@ class Cli {
         if (answers.vehicleType === 'Car') {
           // create a car
           this.createCar();
+        } else if (answers.vehicleType === 'Truck') {
+          this.createTruck();
+        } else {
+          this.createMotorbike();
         }
         // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
       });
