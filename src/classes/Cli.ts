210209be35d2f1ce5bd10cf4bc32c66355edec58
Colton Doling <cmdoling@gmail.com>
2024-11-10 19:40:08 -0500

    completed the findVehicleToTow method to allow a truck to tow a vehicle

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -274,7 +274,7 @@ class Cli {
 
   // method to find a vehicle to tow
   // TODO: add a parameter to accept a truck object
-  findVehicleToTow(): void {
+  findVehicleToTow(truck: Truck): void {
     inquirer
       .prompt([
         {
@@ -293,6 +293,12 @@ class Cli {
         // TODO: check if the selected vehicle is the truck
         // TODO: if it is, log that the truck cannot tow itself then perform actions on the truck to allow the user to select another action
         // TODO: if it is not, tow the selected vehicle then perform actions on the truck to allow the user to select another action
+        if (answers.choices === truck) {
+          console.log(`The truck cannot tow itself..`);
+        } else {
+          truck.tow(answers.value);
+          this.performActions();
+        };
       });
   }
 
